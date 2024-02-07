fb726332fef8aba090c0b0979f888ba12c2642f9
Alamay <khvechuksergiyko@gmail.com>
2024-02-07 19:30:52 +0200

    Added forget password (v0.1)

--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -1,16 +1,18 @@
-import { model, models, Schema } from "mongoose";
+import {model, models, Schema} from "mongoose";
 import bcrypt from "bcrypt"
 
 const UserSchema = new Schema({
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
     name: {type: String, required: true},
     address: {type: String},
     phone: {type: String},
     admin: {type: Boolean, default: false},
-});
+    resetToken: {type: String, required: false},
+    resetTokenExpiry: {type: String, required: false}
+}, {timestamps: true});
 
-UserSchema.post('validate', function (user){
+UserSchema.post('validate', function (user) {
     const notHashedPassword = user.password;
     const salt = bcrypt.genSaltSync(10);
     user.password = bcrypt.hashSync(notHashedPassword, salt);
